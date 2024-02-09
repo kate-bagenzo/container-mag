c51762eb574462d44bdb21f52ac27c2c4d38be52
Kate Bagenzo <katebagenzo@gmail.com>
2024-02-09 13:57:38 +0100

    global sass

---
+++ src/components/GalleryControls.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <ol>
+        <li>
+            <button>&lt;</button>
+        </li>
+        <li>
+            <button>&gt;</button>
+        </li>
+    </ol>
+</template>
+
+<style scoped lang="scss">
+    ol {
+        list-style-type: none;
+        display: flex;
+
+        button {
+            all: unset;
+            color: $core-coral;
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+    }
+</style>
