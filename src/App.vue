a8d85e9c6285608dcb8a96ed0b1c7d0f5e432650
Kate Bagenzo <katebagenzo@gmail.com>
2024-02-20 13:19:38 +0100

    prep main view

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ import GalleryMainView from './components/GalleryMainView.vue';
     transition: background-color 2s;
   }
 
-  .artview {
+  #intro {
     grid-area: art;
   }
 
--- src/components/GalleryMainView.vue
+++ src/components/GalleryMainView.vue
@@ -4,17 +4,11 @@ import { galleryState } from '@/galleryState';
 </script>
 
 <template>
-  <Transition mode="out-in">
-    <section v-if="galleryState.position == 0" class="artview" id="intro">
+    <section id="intro">
       <h1><em>CONTAINER</em> presents:</h1>
       <h2>[computing in 2024]</h2>
       <h3>gallerie / collection</h3>
     </section>
-
-    <section v-else-if="galleryState.position == 1" class="artview" id="flowers">
-      <img src="../assets/img/flowers.png" />
-    </section>
-  </Transition>
 </template>
 
 <style scoped lang=scss>
