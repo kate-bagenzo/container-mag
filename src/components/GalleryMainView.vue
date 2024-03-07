1d9ba356e333b26cc540a3126860dd97062ae523
Kate Bagenzo <katebagenzo@gmail.com>
2024-03-07 20:33:19 +0100

    main images

--- src/components/GalleryMainView.vue
+++ src/components/GalleryMainView.vue
@@ -16,11 +16,11 @@ import { galleryState } from '@/galleryState';
           <h2>[computing in 2024]</h2>
           <h3>gallerie / collection</h3>
         </section>
-        <section class="piece img_flowers" v-else-if="galleryState.position == 1"></section>
-        <section class="piece img_angel" v-else-if="galleryState.position == 2"></section>
-        <section class="piece img_stretch" v-else-if="galleryState.position == 3"></section>
-        <section class="piece img_user" v-else-if="galleryState.position == 4"></section>
-        <section class="piece img_hill" v-else-if="galleryState.position == 5"></section>
+        <img class="piece" src="../assets/img/flowers.png" v-else-if="galleryState.position == 1"></img>
+        <img class="piece" src="../assets/img/angel.png" v-else-if="galleryState.position == 2"></img>
+        <img class="piece" src="../assets/img/stretch.png" v-else-if="galleryState.position == 3"></img>
+        <img class="piece" src="../assets/img/user.png" v-else-if="galleryState.position == 4"></img>
+        <img class="piece" src="../assets/img/hill.png" v-else-if="galleryState.position == 5"></img>
       </Transition>
     </section>
 </template>
@@ -84,29 +84,6 @@ import { galleryState } from '@/galleryState';
   .piece {
     width: 100%;
     height: 100%;
-    background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-
-
-  .img_flowers {
-    background-image: url('../assets/img/flowers.png');
-  }
-
-  .img_angel {
-    background-image: url('../assets/img/angel.png');
-  }
-
-  .img_stretch {
-    background-image: url('../assets/img/stretch.png');
-  }
-
-  .img_user {
-    background-image: url('../assets/img/user.png');
-  }
-
-  .img_hill {
-    background-image: url('../assets/img/hill.png');
+    object-fit: contain;
   }
 </style>
